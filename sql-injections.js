52112db0b68fd1ab938fec9f04fa906ce5b85f74
raphael martinez <root@localhost.localdomain>
2018-12-10 22:40:31 -0500

    inital-commit

---
+++ sql-injections.js
@@ -0,0 +1,14 @@
+function hasSql(value) {
+    if (value === null || value === undefined) {
+        return true;
+    }
+
+    re = /(insert|delete|<|select|drop|where|orderBy|join)/i
+    if (re.test(value)) {
+        return true
+    }
+    return false;
+}
+
+
+module.exports = hasSql;
